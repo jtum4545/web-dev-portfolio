04703e74585c8f621d930538e8e33fddda27eb73
jtum4545 <JOB.TUMIBAY22@bathspa.ac.uk>
2023-05-04 11:52:35 +0100

    Loading Animation Scrapped

--- index.js
+++
@@ -1,9 +0,0 @@
-const loadingAnimation = document.getElementById("loading-animation");
-const page = document.getElementById("page");
-
-page.style.display = "none";
-
-setTimeout(function(){
-    loadingAnimation.style.display = "none";
-    page.style.display = "block"
-}, 3000)
