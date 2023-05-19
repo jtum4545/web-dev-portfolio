ea38fc9dd95f538d55f1e58ad1592046ccbd87ce
jtum4545 <JOB.TUMIBAY22@bathspa.ac.uk>
2023-05-19 19:54:52 +0100

    Projects page finished (for now...)

--- pages-scripts/projects.js
+++ pages-scripts/projects.js
@@ -1,24 +1,17 @@
 const previousButton = document.querySelector('.previous-button');
 const nextButton = document.querySelector('.next-button');
 const carouselItems = document.querySelectorAll('.project');
+const carouselText = document.querySelectorAll('.project-text');
 
 let currentIndex = 0;
 let currentPosition = 0;
 
-function carouselNext() {
-    currentIndex -= 1;
-    if (currentIndex == -1) {
-        currentIndex = 0;
-    } else {
-        currentPosition += 500;
-        for(let i = 0; i < 5; i++){
-            carouselItems[i].style.transform = `translateX(${currentPosition}px)`;
-        }
-    }
-    console.log(currentIndex)
+for (let i = 1; i < 5; i++){
+    carouselText[i].style.opacity = '0';
 }
 
-function carouselPrevious() {
+function carouselNext() {
+
     currentIndex += 1;
     if (currentIndex == 5) {
         currentIndex = 4;
@@ -27,6 +20,28 @@ function carouselPrevious() {
         for(let i = 0; i < 5; i++){
             carouselItems[i].style.transform = `translateX(${currentPosition}px)`;
         }
+        carouselText[currentIndex - 1].style.opacity = '0';
+
+        carouselText[currentIndex].style.opacity = '1';
+
     }
     console.log(currentIndex);
 }
+
+function carouselPrevious() {
+    currentIndex -= 1;
+    if (currentIndex == -1) {
+        currentIndex = 0;
+    } else {
+        currentPosition += 500;
+        for(let i = 0; i < 5; i++){
+            carouselItems[i].style.transform = `translateX(${currentPosition}px)`;
+        }
+        carouselText[currentIndex + 1].style.opacity = '0';
+
+        carouselText[currentIndex].style.opacity = '1';
+    }
+    console.log(currentIndex);
+
+}
+
