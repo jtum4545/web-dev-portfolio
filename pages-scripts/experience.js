5922bdbc06a08e2f069a8de3b65d6321ffaf76ef
jtum4545 <JOB.TUMIBAY22@bathspa.ac.uk>
2023-05-21 21:20:15 +0100

    Added visibility for current images shown

--- pages-scripts/experience.js
+++ pages-scripts/experience.js
@@ -3,12 +3,25 @@ const curtainLeft = document.querySelector('.curtain-left');
 const curtainRight = document.querySelector('.curtain-right');
 const images = document.querySelectorAll('.image');
 
-
+openButton[0].style.backgroundColor = 'gray';
 
 openButton.forEach(button => {
   button.addEventListener('click', () => {
     curtainLeft.classList.add('open');
     curtainRight.classList.add('open');
+    if(button.classList.contains("1")) {
+        openButton[0].style.backgroundColor = 'gray';
+        openButton[1].style.backgroundColor = 'white';
+        openButton[2].style.backgroundColor = 'white';
+    } else if (button.classList.contains("2")) {
+        openButton[0].style.backgroundColor = 'white';
+        openButton[1].style.backgroundColor = 'gray';
+        openButton[2].style.backgroundColor = 'white';
+    } else if (button.classList.contains("3")) {
+        openButton[0].style.backgroundColor = 'white';
+        openButton[1].style.backgroundColor = 'white';
+        openButton[2].style.backgroundColor = 'gray';
+    } 
     setTimeout(() => {
         for(let i = 0; i < 3; i++){
             images[i].style.display = 'block';
